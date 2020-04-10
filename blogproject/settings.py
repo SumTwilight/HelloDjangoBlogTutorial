9027137bb9da881ee749948672bcb394effd3dc9
SummerTwilight <ncutzl@qq.com>
2020-04-10 21:17:20 +0800

    '修改sitting,删除db,上传到阿里云'

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h&uu*w%quu+%&ayrncz9vi9bp8dy39#86huay_xd_e2d=2_%6h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com']
 
 
 # Application definition
@@ -122,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# 加入下面的配置
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+

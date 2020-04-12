f8a9172e9ceb2d534a36e7fbc8ec9c4c5a8e5b8d
SummerTwilight <ncutzl@qq.com>
2020-04-12 15:52:26 +0800

    关闭debug模式

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h&uu*w%quu+%&ayrncz9vi9bp8dy39#86huay_xd_e2d=2_%6h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com', '120.26.178.160']
 

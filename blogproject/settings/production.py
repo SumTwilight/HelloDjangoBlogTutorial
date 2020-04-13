3ff5e89258cae24d395206bd5694798282f4fdf9
SummerTwilight <ncutzl@qq.com>
2020-04-13 15:09:48 +0800

    '添加域名SummerTwilight.com'

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -2,4 +2,4 @@ from .common import *
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = False
-ALLOWED_HOSTS = ['HelloDjangoBlogTutorial.ncutzl.com', '120.26.178.160']
+ALLOWED_HOSTS = ['HelloDjangoBlogTutorial.ncutzl.com', '120.26.178.160', 'SummerTwilight.com']

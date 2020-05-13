d476fb62cf17c6628c93327dbd7be4297a93a4ad
SummerTwilight <ncutzl@qq.com>
2020-05-13 22:09:59 +0800

    添加RSS订阅订阅

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,12 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
 
+from markdown.extensions.toc import TocExtension
+from django.template.defaultfilters import slugify
+from django.utils.functional import cached_property
+
 import markdown
+import re
 from django.utils.html import strip_tags
 
 
@@ -42,6 +47,21 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def generate_rich_content(value):
+    md = markdown.Markdown(
+        extensions=[
+            "markdown.extensions.extra",
+            "markdown.extensions.codehilite",
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ]
+    )
+    content = md.convert(value)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    toc = m.group(1) if m is not None else ""
+    return {"content": content, "toc": toc}
 
 
 class Post(models.Model):
@@ -122,3 +142,16 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
+
+    @property
+    def toc(self):
+        return self.rich_content.get("toc", "")
+
+    @property
+    def body_html(self):
+        return self.rich_content.get("content", "")
+
+    @cached_property
+    def rich_content(self):
+        return generate_rich_content(self.body)
+
